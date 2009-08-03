59b79224bef48e834fb974c09c078983f1846737
Daniel <d@lordofhosts.de>
2009-08-03 08:16:43 +0200

    adapt twsh.py to renaming and change of configuration layout

--- twsh.py
+++ twsh.py
@@ -1,11 +1,11 @@
 #!/opt/local/bin/python
 
 import console
-import fileIO
+import configuration
 import os
 
 homedir = os.path.expanduser("~")
-configurator = fileIO.Configuration(homedir+"/.twshrc")
-configuration = configurator.getUserInfo()
-cli = console.Console(configuration['user'],configuration['password'])
+config = configuration.Configuration(homedir+"/.twshrc")
+conf = config.get_configuration()
+cli = console.Console(conf)
 cli.cmdloop()
